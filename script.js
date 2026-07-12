4c93c32c4294969282792b1855978b523097da79
carladar <egizatullina100@gmail.com>
2026-07-11 22:48:56 -0700

    Finished invitation website

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-const button = document.querySelector("button");
+const screen1 = document.getElementById("screen1");
+const screen2 = document.getElementById("screen2");
+let attempts = 0;
 
-const invite = document.getElementById("invite");
+const yesButton = document.getElementById("yes");
 
-button.onclick = function(){
+yesButton.onclick = function () {
 
-    invite.style.display = "block";
+    screen1.style.display = "none";
 
+    screen2.style.display = "flex";
+
+};
+
+const noButton = document.getElementById("no");
+
+noButton.addEventListener("mouseenter", function () {
+    attempts++;
+    if (attempts >= 6) {
+
+    noButton.style.display = "none";
+
+   const message = document.getElementById("message");
+
+message.textContent = "️ Отказы не принимаются 🤭️";
+
+message.style.opacity = "1";
+    yesButton.style.left = "90px";
+    return;
 }
+    noButton.style.position = "absolute";
+
+    const x = Math.random() * 300;
+    const y = Math.random() * 200;
+
+    noButton.style.left = x + "px";
+    noButton.style.top = y + "px";
+
+});
